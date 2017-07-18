75166410370cbe8ae46ceee0d9e10f2537aea9d5
dhingratul <atul.dhingra@rutgers.edu>
2017-07-18 10:32:54 -0400

    correction

--- plotDataSlice.py
+++ plotDataSlice.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 def plot_selected(df, columns, start_index, end_index):
     """Plot the desired columns over index values in the given range."""
-    plot_data(df.ix(start_index:end_index, columns))
+    plot_data(df.ix[start_index:end_index, columns])
 
 
 def symbol_to_path(symbol, base_dir="data"):
